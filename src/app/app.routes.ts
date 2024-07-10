b018c3dd8ba2c3391178ff864762880b5faa236f
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-07-10 17:12:13 +0200

    first-commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPagesComponent } from './shared/pages/contact-pages/contact-pages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'contact', component: ContactPagesComponent },
+  { path: '**', redirectTo: 'home' }
+];
---
+++ src/app/shared/pages/contact-pages/contact-pages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-pages',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-pages.component.html',
+  styleUrl: './contact-pages.component.css'
+})
+export class ContactPagesComponent {
+
+}
